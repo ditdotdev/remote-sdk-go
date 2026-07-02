f71dd5c8448dc69dbc05c47707d396f5eb725180
Robert Eric Reeves <robertericreeves@gmail.com>
2026-07-02 14:16:35 -0500

    Update stale Datadatdat brand references in package doc comments

--- remote/util.go
+++ remote/util.go
@@ -1,7 +1,7 @@
 // Copyright Dit 2026
 // SPDX-License-Identifier: BUSL-1.1
 
-// Package remote provides the core remote plugin infrastructure for Datadatdat.
+// Package remote provides the core remote plugin infrastructure for Dit.
 package remote
 
 import (
